966cc29ecb025fbf89943b8a28733b7c33c18a2b
Diogo Passos <dev@diogopassos.pt>
2019-11-16 18:05:48 +0000

    Added some styiling - navbar and hero section

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Nozama</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/welcome.css">
+    </head>
+    <body>
+        <nav class="navbar">
+            <a class="logo" href="/"><img src="img/logo.png" alt=""></a>
+            <ul>
+                @auth
+                    <li><a class="btn inverted" href="{{ route('login') }}"><i class="fas fa-user"></i> Member Area</a></li>
+                @else
+                    <li><a class="btn inverted" href="{{ route('login') }}"><i class="fas fa-user"></i> Entrar</a></li>
+                @endauth
+                
+                <li><a class="btn primary"  href="#">Comprar</a></li>
+            </ul>
+        </nav>
+        @yield('content')
+
+        <footer>
+        </footer>
+    </body>
+</html>
