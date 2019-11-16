cc334b2c3ee3cd5d1729fdbef55ea9471dc82688
Diogo Passos <dev@diogopassos.pt>
2019-11-16 18:21:09 +0000

    updated welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.home')
 
-        <title>Nozama</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/welcome.css">
-    </head>
-    <body>
-        <nav class="navbar">
-            <a class="logo" href="#"><img src="img/logo.png" alt=""></a>
-            <ul>
-                <li><a class="btn inverted" href="#"><i class="fas fa-user"></i> Entrar</a></li>
-                <li><a class="btn primary"  href="#">Comprar</a></li>
-            </ul>
-        </nav>
-    </body>
-</html>
+@section('content')
+    <div class="hero img">
+        <h1 class="title">Uma nova maneira de vender e comprar!</h1>
+        <h2 class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+    </div>
+@endsection
