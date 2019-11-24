8d0c438e6d5577b4a90222682c01d37f4cde72a9
Diogo Passos <dev@diogopassos.pt>
2019-11-24 18:33:41 +0000

    added user seeder/factory

---
+++ database/migrations/2019_11_24_160804_create_proposals_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProposalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->text('description');
+            $table->decimal('price',10,2);
+            $table->enum('state',['used', 'new']);
+
+            // foreign keys
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->bigInteger('purchase_id')->unsigned();
+            $table->foreign('purchase_id')->references('id')->on('purchases')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposals');
+    }
+}
