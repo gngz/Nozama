4ec222957898b046d43716db23e8e406af83f5f5
Alex Faria <alex.faria@me.com>
2019-12-30 23:17:12 +0000

    day 1 commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,41 @@
 @extends('layouts.home')
 
 @section('content')
-    <div class="hero img">
-        <h1 class="title">Uma nova maneira de vender e comprar!</h1>
-        <h2 class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+<div class="hero img">
+    <h1 class="title">Uma nova maneira de vender e comprar!</h1>
+    <h2 class="subtitle">De Z a A, anuncie a sua próxima compra!</h2>
+    <h4 class="subsubtitle">Ao contrário das outras plataformas, o Nozama não lhe pergunta o que quer vender,
+        <br>pergunta o que quer comprar.<br><br><br></h4>
+    <button class="btn flat-primary" style="font-size: 15px;">Anunciar</button>
+</div>
+<div class="contents_homepage">
+    <div class="highlights">
+        <div class="highlights_title title">
+            Destaques
+        </div>
+        <div class="highlights_contents">
+            <div class=highlights_images>
+                <div class=highlights_image_1>
+                    <img src="https://via.placeholder.com/200x300?text=Placeholder+Image" alt="">
+                </div>
+                <div class=highlights_image_2>
+                    <img src="https://via.placeholder.com/200x300?text=Placeholder+Image" alt="">
+                </div>
+                <div class=highlights_image_3>
+                    <img src="https://via.placeholder.com/200x300?text=Placeholder+Image" alt="">
+                </div>
+                <div class=highlights_image_4>
+                    <img src="https://via.placeholder.com/200x300?text=Placeholder+Image" alt="">
+                </div>
+                <div class=highlights_image_5>
+                    <img src="https://via.placeholder.com/200x300?text=Placeholder+Image" alt="">
+                </div>
+            </div>
+        </div>
     </div>
+    <div>
+        <!--Headlines-->
+    </div>
+</div>
+
 @endsection
