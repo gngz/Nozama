1fd3f995bcce0b207e473f3115ec7d1a1c4b82ec
Diogo Passos <dev@diogopassos.pt>
2019-12-31 12:57:12 +0000

    Fix purchase controller

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -30,11 +30,12 @@ class PurchaseController extends Controller
 
         $purchase = Purchase::find($request->id);
 
-        $proposals = $purchase->proposals;
+        
 
 
         
         if($purchase) {
+            $proposals = $purchase->proposals;
             $images = $purchase->images();
             return view("purchase.view",["user" => $user,"purchase" =>  $purchase, "images" => $images, "proposals" => $proposals]);
         } else {
