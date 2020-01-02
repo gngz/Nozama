72b37f5764e7f5c8735abe073ba04778a697ffc8
Diogo Passos <dev@diogopassos.pt>
2020-01-02 01:32:21 +0000

    Criação dos Termos e Condições e Politica de Privacidade

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,126 @@
+@extends('layouts.home')
+
+@section('content')
+
+<div class="row centered">
+    <div class="sm col-8">
+        <h1 class="title">Termos e Condições</h1>
+    </div>
+</div>
+<div class="row centered">
+    <div class="sm col-8">
+    
+    <h2 class="title">VISÃO GERAL</h2>
+
+    <p>Esse site é operado pela Nozama. Em todo o site, os termos “nós”, “nos” e “nosso” se referem à Nozama. A Nozama proporciona este site, incluindo todas as informações, ferramentas e serviços disponíveis deste site para si, o utilizador, com a condição da sua aceitação de todos os termos, condições, políticas e avisos declarados aqui.</p>
+    <p>Ao visitar nosso site e/ou comprar alguma coisa no nosso site, você está utilizando nossos “Serviços”. Consequentemente, você  concorda com os seguintes termos e condições , incluindo os termos e condições e políticas adicionais mencionados neste documento e/ou disponíveis por hyperlink. Esses Termos de serviço se aplicam a todos os utilizadores do site, incluindo, sem limitação, os utilizadores que são navegadores, clientes, vendedores, compradores e/ou contribuidores de conteúdo.</p>
+    <p>Por favor, leia estes Termos e Condições cuidadosamente antes de aceder ou utilizar o nosso site. Ao aceder ou usar qualquer parte do site, você concorda com os Termos e Condições. Se você não concorda com todos os termos e condições desse acordo, então você não pode aceder ao site ou usar quaisquer serviços. Se esses Termos e Condições são considerados uma oferta, a aceitação é expressamente limitada a esses Termos e Condições.</p>
+    <p>Quaisquer novos recursos ou ferramentas que forem adicionados à plataforma atual também devem estar sujeitos aos Termos e Condições. Você pode rever a versão mais atual dos Termos e Condições quando quiser nesta página. Reservamos o direito de atualizar, alterar ou trocar qualquer parte desses Termos e Condições ao publicar atualizações e/ou alterações no nosso site. É sua responsabilidade verificar as alterações feitas nesta página periodicamente. Seu uso contínuo ou acesso ao site após a publicação de quaisquer alterações constitui aceitação de tais alterações.</p>
+
+    <h2 class="title">SEÇÃO 1 - TERMOS DA PLATAFORMA</h2>
+    
+    <p>Ao concordar com os Termos e Condições, você confirma que você é maior de idade em seu estado ou província de residência e que você nos deu seu consentimento para permitir que qualquer um dos seus dependentes menores de idade usem esse site.</p>
+    <p>Você não deve usar nossos produtos para qualquer fim ilegal ou não autorizado. Você também não pode, ao usufruir deste Serviço, violar quaisquer leis em sua jurisdição (incluindo, mas não limitado, a leis de direitos de autor).</p>
+    <p>Você não deve transmitir nenhum vírus ou qualquer código de natureza destrutiva.</p>
+    <p>Violar qualquer um dos Termos tem como consequência a rescisão imediata dos seus Serviços.</p>
+
+    <h2 class="title">SEÇÃO 2 - CONDIÇÕES GERAIS</h2>
+    
+    <p>Reservamos o direito de recusar o serviço a qualquer pessoa por qualquer motivo a qualquer momento.</p>
+    <p>Você entende que o seu conteúdo (não incluindo informações de cartão de crédito), pode ser transferido sem criptografia e pode: (a) ser transmitido por várias redes; e (b) sofrer alterações para se adaptar e se adequar às exigências técnicas de conexão de redes ou dispositivos. As informações de cartão de crédito sempre são criptografadas durante a transferência entre redes.</p>
+    <p>Você concorda em não reproduzir, duplicar, copiar, vender, revender ou explorar qualquer parte do Serviço, uso do Serviço, acesso ao Serviço, ou qualquer contato no site através do qual o serviço é fornecido, sem nossa permissão expressa por escrito.</p>
+    <p>Os títulos usados neste acordo são incluídos apenas por conveniência e não limitam ou  afetam os Termos e Condições.</p>
+
+    <h2 class="title">SEÇÃO 3 - PRECISÃO, INTEGRIDADE E ATUALIZAÇÃO DAS INFORMAÇÕES</h2>
+
+    <p>Não somos responsáveis por informações disponibilizadas nesse site que não sejam precisas, completas ou atuais. O material desse site é fornecido apenas para fins informativos e não deve ser usado como a única base para tomar decisões sem consultar fontes de informações primárias, mais precisas, mais completas ou mais atuais. Qualquer utilização do material desse site é por sua conta e risco.</p>
+    <p>Esse site pode conter certas informações históricas. As informações históricas podem não ser atuais e são fornecidas apenas para sua referência. Reservamos o direito de modificar o conteúdo desse site a qualquer momento, mas nós não temos obrigação de atualizar nenhuma informação em nosso site. Você concorda que é de sua responsabilidade monitorar alterações no nosso site.</p>
+    
+    <h2 class="title">SEÇÃO 4 - MODIFICAÇÕES DO SERVIÇO E PREÇOS</h2>
+
+    <p>Os preços dos produtos são sujeitos a alterações sem notificação.</p>
+    <p>Reservamos o direito de, a qualquer momento, modificar ou descontinuar o Serviço (ou qualquer parte ou conteúdo do mesmo) sem notificação em qualquer momento.</p>
+    <p>Não nos responsabilizados por você ou por qualquer terceiro por qualquer modificação, alteração de preço, suspensão ou descontinuação do Serviço.</p>
+
+    <h2 class="title">SEÇÃO 5 - PRODUTOS OU SERVIÇOS (caso aplicável)</h2>
+
+    <p>Não garantimos que a qualidade de quaisquer produtos, serviços, informações ou outros materiais comprados ou obtidos por você vão atender às suas expectativas, ou que quaisquer erros no Serviço serão corrigidos.</p>
+
+    <h2 class="title">SEÇÃO 6 - PRECISÃO DAS INFORMAÇÕES DE FACTURAÇÃO E CONTA</h2>
+
+    <p>Você concorda em fornecer suas informações de conta e compra completas para todas as compras feitas na nossa plataforma. Você concorda em atualizar prontamente sua conta e outras informações, incluindo seu e-mail, números de cartão de crédito e datas de validade, para que possamos completar suas transações e contactar você quando preciso.</p>
+
+    <h2 class="title">SEÇÃO 7 - FERRAMENTAS OPCIONAIS</h2>
+
+    <p>Podemos dar-lhe acesso a ferramentas de terceiros que não monitorizamos e nem temos qualquer controlo.</p>
+    <p>Você reconhece e concorda que nós fornecemos acesso a tais ferramentas ”como elas são” e “conforme a disponibilidade” sem quaisquer garantias, representações ou condições de qualquer tipo e sem qualquer endosso. Não nos responsabilizamos de forma alguma pelo seu uso de ferramentas opcionais de terceiros.</p>
+    <p>Qualquer uso de ferramentas opcionais oferecidas através do site é inteiramente por sua conta e risco e você se familiarizar e aprovar os termos das ferramentas que são fornecidas por fornecedor(es) terceiro(s).</p>
+    <p>Também podemos, futuramente, oferecer novos serviços e/ou recursos através do site (incluindo o lançamento de novas ferramentas e recursos). Tais recursos e/ou serviços novos também devem estar sujeitos a esses Termos e Condições.</p>
+
+    <h2 class="title">SEÇÃO 8 - LINKS DE TERCEIROS</h2>
+    <p>Certos produtos, conteúdos e serviços disponíveis pelo nosso Serviço podem incluir materiais de terceiros.</p>
+    <p>Os links de terceiros neste site podem te direcionar para sites de terceiros que não são afiliados a nós. Não nos responsabilizamos por examinar ou avaliar o conteúdo ou precisão. Não garantimos e nem temos obrigação ou responsabilidade
+    <p>Não somos responsáveis por quaisquer danos ou prejuízos relacionados com a compra ou uso de mercadorias, serviços, recursos, conteúdo, ou quaisquer outras transações feitas em conexão com quaisquer sites de terceiros. Por favor, reveja com cuidado as políticas e práticas de terceiros e certifique-se que você as entende antes de efetuar qualquer transação. As queixas, reclamações, preocupações ou questões relativas a produtos de terceiros devem ser direcionadas ao terceiro.</p>
+
+    <h2 class="title">SEÇÃO 9 - COMENTÁRIOS, FEEDBACK, ETC. DO UTILIZADOR</h2>
+    <p>Se, a nosso pedido, você enviar certos itens específicos (por exemplo, participação em um concurso), ou sem um pedido nosso, você enviar ideias criativas, sugestões, propostas, planos, ou outros materiais, seja online, por e-mail, pelo correio, ou de outra forma (em conjunto chamados de 'comentários'), você concorda que podemos, a qualquer momento, sem restrição, editar, copiar, publicar, distribuir, traduzir e de outra forma usar quaisquer comentários que você encaminhe para nós. Não nos responsabilizamos por: (1) manter quaisquer comentários em sigilo; (2) indemnizar por quaisquer comentários; ou (3) responder quaisquer comentários.</p>
+    <p>Podemos, mas não temos a obrigação, de monitorizar, editar ou remover conteúdo que nós determinamos a nosso próprio critério ser contra a lei, ofensivo, ameaçador, calunioso, difamatório, pornográfico, obsceno ou censurável ou que viole a propriedade intelectual de terceiros ou estes Termos e Condições.</p>
+    <p>Você concorda que seus comentários não violarão qualquer direito de terceiros, incluindo direitos de autor, marcas registadas, privacidade, personalidade ou outro direito pessoal ou de propriedade. Você concorda que os seus comentários não vão conter material difamatório, ilegal, abusivo ou obsceno. Eles também não conterão nenhum vírus de computador ou outro malware que possa afetar a operação do Serviço ou qualquer site relacionado. Você não pode usar um endereço de e-mail falso, fingir ser alguém diferente de si mesmo, ou de outra forma enganar a nós ou terceiros quanto à origem de quaisquer comentários. Você é o único responsável por quaisquer comentários que você faz e pela veracidade deles. Nós não assumimos qualquer responsabilidade ou obrigação por quaisquer comentários publicados por você ou por qualquer terceiro.</p>
+
+    <h2 class="title">SEÇÃO 10 - INFORMAÇÕES PESSOAIS</h2>
+
+    <p>O envio de suas informações pessoais através da plataforma é regido pela nossa Política de privacidade. <a href="{{ url('/privacy')}}">Ver nossa Política de privacidade</a>.</p>
+
+    <h2 class="title">SEÇÃO 11 - ERROS, IMPRECISÕES E OMISSÕES</h2>
+
+    <p>Ocasionalmente, pode haver informações no nosso site ou no Serviço que contém erros tipográficos, imprecisões ou omissões que possam relacionar-se a descrições de produtos, preços, promoções, ofertas, taxas de envio do produto, o prazo de envio e disponibilidade. Reservamos o direito de corrigir quaisquer erros, imprecisões ou omissões, e de alterar ou atualizar informações ou cancelar encomendas caso qualquer informação no Serviço ou em qualquer site relacionado seja imprecisa, a qualquer momento e sem aviso prévio (até mesmo depois de você ter enviado o seu pedido).</p>
+    <p>Não assumimos nenhuma obrigação de atualizar, alterar ou esclarecer informações no Serviço ou em qualquer site relacionado, incluindo, sem limitação, a informações sobre preços, exceto conforme exigido por lei. Nenhuma atualização específica ou data de atualização no Serviço ou em qualquer site relacionado, deve ser utilizada para indicar que todas as informações do Serviço ou em qualquer site relacionado tenham sido modificadas ou atualizadas.</p>
+
+    <h2 class="title">SEÇÃO 12 - USOS PROIBIDOS</h2>
+
+    <p>Além de outras proibições, conforme estabelecido nos Termos de serviço, você está proibido de usar o site ou o conteúdo para: (a) fins ilícitos; (b) solicitar outras pessoas a realizar ou participar de quaisquer atos ilícitos; (c) violar quaisquer regulamentos internacionais, provinciais, estaduais ou federais, regras, leis ou regulamentos locais; (d) infringir ou violar nossos direitos de propriedade intelectual ou os direitos de propriedade intelectual de terceiros; (e) para assediar, abusar, insultar, danificar, difamar, caluniar, depreciar, intimidar ou discriminar com base em gênero, orientação sexual, religião, etnia, raça, idade, nacionalidade ou deficiência; (f) apresentar informações falsas ou enganosas; (g) fazer o envio ou transmitir vírus ou qualquer outro tipo de código malicioso que será ou poderá ser utilizado para afetar a funcionalidade ou operação do Serviço ou de qualquer site relacionado, outros sites, ou da Internet; (h) coletar ou rastrear as informações pessoais de outras pessoas; (i) para enviar spam, phishing, pharm, pretext, spider, crawl, ou scrape; (j) para fins obscenos ou imorais; ou (k) para interferir ou contornar os recursos de segurança do Serviço ou de qualquer site relacionado, outros sites, ou da Internet. Reservamos o direito de rescindir o seu uso do Serviço ou de qualquer site relacionado por violar qualquer um dos usos proibidos.</p>
+
+    <h2 class="title">SEÇÃO 13 - ISENÇÃO DE RESPONSABILIDADE DE GARANTIAS; LIMITAÇÃO DE RESPONSABILIDADE</h2>
+    
+    <p>Nós não garantimos, representamos ou justificamos que o seu uso do nosso serviço será pontual, seguro, sem erros ou interrupções.</p>
+    <p>Não garantimos que os resultados que possam ser obtidos pelo uso do serviço serão precisos ou confiáveis.</p>
+    <p>Você concorda que de tempos em tempos, podemos remover o serviço por períodos indefinidos de tempo ou cancelar a qualquer momento, sem lhe notificar.</p>
+    <p>Você concorda que o seu uso ou incapacidade de usar o serviço é por sua conta e risco. O serviço e todos os produtos e serviços entregues através do serviço são, exceto conforme declarado por nós) fornecidos sem garantia e conforme a disponibilidade para seu uso, sem qualquer representação, garantias ou condições de qualquer tipo, expressas ou implícitas, incluindo todas as garantias implícitas ou condições de comercialização, quantidade, adequação a uma finalidade específica, durabilidade, título, e não violação.</p>
+    <p>Em nenhuma circunstância a Nozama, nossos diretores, oficiais, funcionários, afiliados, agentes, contratantes, estagiários, fornecedores, prestadores de serviços ou licenciadores serão responsáveis por qualquer prejuízo, perda, reclamação ou danos diretos, indiretos, incidentais, punitivos, especiais ou consequentes de qualquer tipo, incluindo, sem limitação, lucros cessantes, perda de receita, poupanças perdidas, perda de dados, custos de reposição, ou quaisquer danos semelhantes, seja com base em contrato, ato ilícito (incluindo negligência), responsabilidade objetiva ou de outra forma, decorrentes do seu uso de qualquer um dos serviços ou quaisquer produtos adquiridos usando o serviço, ou para qualquer outra reclamação relacionada de alguma forma ao seu uso do serviço ou qualquer produto, incluindo, mas não limitado a, quaisquer erros ou omissões em qualquer conteúdo, ou qualquer perda ou dano de qualquer tipo como resultado do uso do serviço ou qualquer conteúdo (ou produto) publicado, transmitido ou de outra forma disponível através do serviço, mesmo se alertado ​​de tal possibilidade. Como alguns estados ou jurisdições não permitem a exclusão ou a limitação de responsabilidade por danos consequentes ou incidentais, em tais estados ou jurisdições, a nossa responsabilidade será limitada à extensão máxima permitida por lei.</p>
+
+    <h2 class="title">SEÇÃO 14 - INDEMNIZAÇÃO</h2>
+
+    <p>Você concorda em indemnizar, defender e isentar a Nozama e nossos subsidiários, afiliados, parceiros, funcionários, diretores, agentes, contratados, licenciantes, prestadores de serviços, subcontratados, fornecedores, estagiários e funcionários, de qualquer reclamação ou demanda, incluindo honorários de advogados, por quaisquer terceiros devido à violação destes Termos e Condições ou aos documentos que incorporam por referência, ou à violação de qualquer lei ou os direitos de um terceiro.</p>
+
+    <h2 class="title">SEÇÃO 15 - INDEPENDÊNCIA</h2>
+
+    <p>No caso de qualquer disposição destes Termos e Condições ser considerada ilegal, nula ou ineficaz, tal disposição deve, contudo, ser aplicável até ao limite máximo permitido pela lei aplicável, e a porção inexequível será considerada separada destes Termos e Condições. Tal determinação não prejudica a validade e aplicabilidade de quaisquer outras disposições restantes.</p>
+
+    <h2 class="title">SEÇÃO 16 - RESCISÃO</h2>
+
+    <p>As obrigações e responsabilidades das partes incorridas antes da data de rescisão devem continuar após a rescisão deste acordo para todos os efeitos.</p>
+    <p>Estes Termos e Condições estão em vigor, a menos que seja rescindido por si ou por nós. Você pode rescindir estes Termos e Condições a qualquer momento, notificando-nos que já não deseja utilizar os nossos serviços, ou quando você deixar de usar o nosso site.</p>
+    <p>Se em nosso critério exclusivo você não cumprir com qualquer termo ou disposição destes Termos e Condições, nós também podemos rescindir este contrato a qualquer momento sem aviso prévio e você ficará responsável por todas as quantias devidas até a data da rescisão; também podemos lhe negar acesso a nossos Serviços (ou qualquer parte deles).</p>
+
+    <h2 class="title">SEÇÃO 17 - ACORDO INTEGRAL</h2>
+
+    <p>Caso não exerçamos ou executemos qualquer direito ou disposição destes Termos e Condições, isso não constituirá uma renúncia de tal direito ou disposição.</p>
+    <p>Estes Termos e Condições e quaisquer políticas ou normas operacionais publicadas por nós neste site ou no que diz respeito ao serviço constituem a totalidade do acordo  entre nós. Estes termos regem o seu uso do Serviço, substituindo quaisquer acordos anteriores ou contemporâneos, comunicações e propostas, sejam verbais ou escritos, entre você e nós (incluindo, mas não limitado a quaisquer versões anteriores dos Termos e Condições).</p>
+    <p>Quaisquer ambiguidades na interpretação destes Termos e Condições não devem ser interpretadas contra a parte que os redigiu.</p>
+
+    <h2 class="title">SEÇÃO 18 - LEGISLAÇÃO APLICÁVEL</h2>
+
+    <p>Esses Termos e Condições e quaisquer acordos separados em que nós lhe fornecemos os Serviços devem ser regidos e interpretados de acordo com as leis da Região Autónoma da Madeira, Portugal.</p>
+
+    <h2 class="title">SEÇÃO 19 - ALTERAÇÕES DOS TERMOS E CONDIÇÕES</h2>
+
+    <p>Você pode rever a versão mais atual dos Termos e Condições a qualquer momento nesta página.</p>
+    <p>Reservamos o direito, a nosso critério, de atualizar, modificar ou substituir qualquer parte destes Termos e Condições ao publicar atualizações e alterações no nosso site. É sua responsabilidade verificar nosso site periodicamente. Seu uso contínuo ou acesso ao nosso site ou ao Serviço após a publicação de quaisquer alterações a estes Termos e Condições constitui aceitação dessas alterações.</p>
+
+    <h2 class="title">SEÇÃO 20 - INFORMAÇÕES DE CONTACTO</h2>
+
+    <p>As perguntas sobre os Termos e Condições devem ser enviadas para nós através do <a href="mailto:mail@nozama.ga">mail@nozama.ga</a>.</p>
+    <br>
+    </div>
+</div>
+@endsection
