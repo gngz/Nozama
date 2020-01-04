2677a7e500babf5c368766d163e441667fc2195d
Diogo Passos <dev@diogopassos.pt>
2020-01-04 19:04:30 +0000

    api doc

--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -5,14 +5,14 @@
 <div class="row centered">
     <div class="sm col-8">
         <h1 class="title">API da Nozama</h1>
-        <h2 class="subtitle">Aqui poderá ver os endpoints da nossa api.</h2>
+        <h2 class="subtitle">Documentação da API da Nozama.</h2>
     </div>
 </div>
 
 <div class="row centered">
     <div class="sm col-8">
-        <p>Esta API necessita de autenticação.</p>
-        <p>Para ter acesso à nossa api, terá pedir acessoa através do seguinte endereço de email: <a href="mailto:api@nozama.ga">api@nozama.ga</a>.</p>
+        <p>Esta API necessita de autenticação e deverá ser passada através de Bearer Token.</p>
+        <p>Para ter acesso à nossa api, terá pedir a chave de autenticação através do seguinte endereço de email: <a href="mailto:api@nozama.ga">api@nozama.ga</a>.</p>
     </div>
 </div>
 
@@ -20,15 +20,14 @@
 <div class="row centered">
     <div class="sm col-8">
         <h2 class="title">Endpoints</h3>
+        <h2 class="subtitle">Poderá ver a documentação e exemplo dos endpoints no link abaixo.</h3>
     </div>
 </div>
 
 
 <div class="row centered">
     <div class="sm col-8">
-        <h2 class="subtitle">Utilizadores</h3>
-        <h3 class="title">Obter todos os utilizadores - {{ route('getUsers') }}</h3>
-        <h3 class="title">Obter todos utilizador especifico - {{ url(route('getUser',""))."/{id}" }}</h3>
+       <a href="https://documenter.getpostman.com/view/6515207/SWLe6nYg?version=latest" class="btn primary">Ver Documentação</a>
     </div>
 </div>
 @endsection
